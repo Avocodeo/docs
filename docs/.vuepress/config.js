e826d6373eff8ab63ededf01e81fc9de32acc545
James Moore <jamesm0025@gmail.com>
2019-09-28 13:18:53 -0400

    documentation repo initial push

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Avocodeo',
+    description: 'Just playin',
+    themeConfig: {
+        smoothScroll: true,
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Guide', link: '/guide/'},
+            {text: 'Group', link: '/group/'},
+            {text: 'External', link: 'https://github.com/Avocodeo'}
+        ],
+        sidebar: 'auto'
+    },
+}
---
+++ docs/group/README.md
@@ -0,0 +1,17 @@
+# Group
+
+## Availability 
+
+| Member        | Available           | Not Available  |
+| ------------- |:-------------:| -----:|
+| James Moore     | After 5pm Monday - Friday / Weekends| Monday-Friday 8am - 4pm |
+| Tyler Ouellette     |       |    |
+| Justin Dearden |       |     |
+| Sahil Verma |       |     |
+| Nick Stanton |       |     |
+
+## Sprints
+
+### Sprint 1 
+
+
